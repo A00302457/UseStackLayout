e256bb7d93bebec5b5d3bcdd27523c5e415a62de
poojagithub <A00302457@mycambian.ca>
2023-11-14 12:49:35 -0700

    Add project files.

---
+++ UseStackLayout/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace UseStackLayout
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ UseStackLayout/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace UseStackLayout
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnNormalTip(object sender, EventArgs e)
+        {
+        }
+        private void OnGenerousTip(object sender, EventArgs e)
+        {
+        }
+    }
+}
